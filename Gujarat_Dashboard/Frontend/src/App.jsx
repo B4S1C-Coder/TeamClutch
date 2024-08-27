731aaa05da606c1a69560cfbd65a18f082a6d57c
NamanmeetSingh <87459124+NamanmeetSingh@users.noreply.github.com>
2024-08-27 10:27:42 +0530

    1020

--- Gujarat_Dashboard/Frontend/src/App.jsx
+++ Gujarat_Dashboard/Frontend/src/App.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import LoaderPart1 from './components/LoaderPart1.jsx'
+import React from "react";
+import LoaderPart1 from "./components/LoaderPart1.jsx";
+import LoaderPart2 from "./components/LoaderPart2.jsx";
 
 function App() {
   return (
     <>
-    <LoaderPart1 />
+      <div className="overflow-x-hidden">
+        <LoaderPart1 />
+        <LoaderPart2 />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ Gujarat_Dashboard/Frontend/src/components/LoaderPart2.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function LoaderPart2() {
+  return (
+    <>
+      <div className="w-[100vw] h-[100vh] bg-white">
+        
+      </div>
+    </>
+  );
+}
+
+export default LoaderPart2;
